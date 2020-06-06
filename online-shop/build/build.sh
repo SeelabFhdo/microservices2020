bbcbd795571220fffeec88b047e41202da93f9fa
Philip Wizenty <philip.wizenty@fh-dortmundde>
2020-06-06 18:27:03 +0200

    Removed generated files.

--- online-shop/build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd "../eureka" && sh build.sh
-cd "../itemservice" && sh build.sh
-cd "../zuul" && sh build.sh
-cd "../customerservice" && sh build.sh
-cd "../orderservice" && sh build.sh
